4cbc97ab440e89828b5934ca8ae3c1ff834f44fc
Francisco Alvarado <fjalvarado.1.2001@gmail.com>
2024-01-24 02:53:56 -0500

    commitwithmoreaboutme

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,17 +12,11 @@ const About = () => {
             About
           </p>
         </div>
-        <p className="text-xl mt-20">
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
-          quisquam dicta itaque minima vel autem sit modi, illo illum earum
-          mollitia corporis ab? Vitae veniam officia nostrum, numquam provident
-          dicta.
-        </p>
+        <p className="text-xl mt-20 text-justify">
+        My name is Francisco Alvarado, and I was born in the beautiful island of Puerto Rico. However, my formative years were spent in the vibrant surroundings of the Dominican Republic, where I developed a deep appreciation for the rich culture and diverse community. From a young age, I found immense joy in working with computers and discovered a natural talent for building things, which laid the foundation for my passion in technology.        </p>
         <br />
-        <p className="text-xl">
-          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Optio fugit
-          aut consequuntur ex fugiat ipsam quas placeat quis maxime accusamus
-          nam voluptas obcaecati odio eos quasi, harum alias quidem atque!
+        <p className="text-xl text-justify">
+        Fast forward to my professional journey, where I proudly serve in the US Army with a focus on scheduling and dispatching tactical aircraft missions. This experience honed my multitasking skills and instilled in me a strong work ethic. Alongside my military commitment, I also delved into customer service and machining, gaining valuable insights and expertise in these domains. I recently pursued training in web development, embracing the ever-evolving landscape of technology. Now, with a diverse background encompassing military service, customer service, and machining, complemented by my academic achievements, I am actively seeking opportunities in Junior Java Development, IT Support, and Software Development. I am open to new challenges, eager to contribute my skills, and excited to make meaningful connections in the world of technology
         </p>
       </div>
     </div>
